a324e83e05e329611c8125f8643308a5d20316fb
prashantsingamsetti1705 <prashantsingamsetti1705@gmail.com>
2025-06-04 14:41:35 +0530

    apiview

--- restapi_2/testapp/views.py
+++ restapi_2/testapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+class TestapiView(APIView):
+    def get(self,request):
+        data={
+            'name':'testapi',
+            'description':'this is atest api view',
+            'status':'sucess'
+        }
+        return Response({'msg':'this is a test api view','data':data})
+    def post(self,request):
+        return Response({'msg':'this is a test api view'})
+    def put(self,request):
+        return Response({'msg':'this is a test api view'})
+    def delete(self,request):
+        return Response({'msg':'this is a test api view'})
+
+    
 # Create your views here.
